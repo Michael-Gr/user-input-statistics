6718eac6d7c2274ebae7dfe3ba051a9cbc57a782
Michael Grein <michaelgrein92@gmail.com>
2016-11-14 16:29:06 -0500

    sum total loop added

---
+++ app.rb
@@ -0,0 +1,30 @@
+puts "Super Add and Average!"
+
+numbers = []
+
+loop do
+  puts "What are your numbers?"
+  number = gets.chomp
+
+  if number == ""
+    puts "Thank you for your numbers! :)"
+    break
+  end
+
+  numbers << number.to_i
+
+end
+
+total = 0
+count = 0
+
+loop do
+  total = total + numbers[count]
+  count = count + 1
+  if count >= numbers.length
+    break
+  end
+
+end
+
+puts "The sum total of your numbers are #{numbers}. The average is #{numbers}."
